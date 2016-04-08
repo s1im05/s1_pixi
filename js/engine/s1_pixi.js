59623c46ffaa3d8425bebcc6c096febd333d3738
Vadim Suleimanov <s1imus05@gmail.com>
2016-04-08 17:04:41 +0300

    Default options

--- js/engine/s1_pixi.js
+++ js/engine/s1_pixi.js
@@ -1,5 +1,15 @@
 (function(w, u){
-    var s1_pixi = {};
+    var s1_pixi = function(id, options){
+        
+        var defaults    = {
+            width:  640,
+            height: 480
+        };
+        
+        options = Object.assign(defaults, options);
+        
+        var el  = w.document.getElementById(id);
+    };
     
     w.s1_pixi   = s1_pixi;
 })(window);
