d6e5f16719633930283a2b8a7909f3a3caa4a5ae
Сулейманов Вадим Ильдарович <vsuleymanov@corp.local>
2016-04-08 16:19:01 +0300

    Initial commit

---
+++ Gruntfile.js
@@ -0,0 +1,37 @@
+// Обязательная обёртка
+module.exports = function(grunt) {
+
+    grunt.initConfig({
+        jshint: {
+            main: {
+                files: []
+            }
+        },
+        concat: {
+            main: {
+                src: [
+                    'js/libs/jquery.js',
+                    'js/mylibs/**/*.js'  // Все JS-файлы в папке
+                ],
+                dest: 'build/scripts.js'
+            }
+        },
+        // Сжимаем
+        uglify: {
+            main: {
+                files: {
+                    // Результат задачи concat
+                    'build/scripts.min.js': '<%= concat.main.dest %>'
+                }
+            }
+        }
+    });
+
+    // Загрузка плагинов, установленных с помощью npm install
+    grunt.loadNpmTasks('grunt-contrib-jshint');
+    grunt.loadNpmTasks('grunt-contrib-concat');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+
+    // Задача по умолчанию
+    grunt.registerTask('default', ['jshint', 'concat', 'uglify']);
+};
---
+++ readme.md
@@ -0,0 +1,5 @@
+Install:
+
+1. node install
+2. bower install
+3. grunt
